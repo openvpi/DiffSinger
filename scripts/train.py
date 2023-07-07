8d501ae47bfcba2cb2bc5c004d0bf4f6dce7deff
autumn <2>
2023-07-07 14:11:20 +0800

    support pre_train model

--- scripts/train.py
+++ scripts/train.py
@@ -1,7 +1,11 @@
 import importlib
 import os
+import pathlib
 import sys
 from pathlib import Path
+import torch
+
+from utils.training_utils import get_latest_checkpoint_path
 
 root_dir = Path(__file__).parent.parent.resolve()
 os.environ['PYTHONPATH'] = str(root_dir)
@@ -15,6 +19,39 @@ set_hparams()
 if hparams['ddp_backend'] == 'nccl_no_p2p':
     print("Disabling NCCL P2P")
     os.environ['NCCL_P2P_DISABLE'] = '1'
+def load_pre_train_model(model,type:str='part'):
+    r""" load pre model
+
+        Args:
+            model :
+            type (str): part mean loading wavenet  full mean loading fs2 and wavenet
+
+        """
+    pre_train_ckpt_path=hparams.get('pre_train_path')
+    if pre_train_ckpt_path is not None:
+        ckpt=torch.load(pre_train_ckpt_path)
+        if type == "part":
+            state_dict = {}
+            for i in ckpt['state_dict']:
+                if 'diffusion' in i:
+                    print(i)
+                    state_dict[i] = ckpt['state_dict'][i]
+            model.load_state_dict(state_dict,strict=False)
+        elif type == "full":
+            ...
+
+
+def load_warp(model):
+    if get_latest_checkpoint_path(pathlib.Path(hparams['work_dir'])) is not None:
+        pass
+        return None
+    type=hparams.get('pre_train_load_type')
+    if type is None:
+        type="part"
+    load_pre_train_model(model=model,type=type)
+
+
+
 
 
 def run_task():
@@ -22,6 +59,8 @@ def run_task():
     pkg = ".".join(hparams["task_cls"].split(".")[:-1])
     cls_name = hparams["task_cls"].split(".")[-1]
     task_cls = getattr(importlib.import_module(pkg), cls_name)
+    load_warp(task_cls)
+
     task_cls.start()
 
 
