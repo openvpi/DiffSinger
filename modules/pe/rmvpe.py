9c68381aed489d8e0b873fe2b1977b44a5cfc5b3
llc1995@sina.com <llc1995@sina.com>
2023-07-24 20:38:09 +0800

    Improve interpolation for rmvpe

--- modules/pe/rmvpe.py
+++ modules/pe/rmvpe.py
@@ -2,17 +2,21 @@ import numpy as np
 from basics.base_pe import BasePE
 from modules.rmvpe.inference import RMVPE as rmvpe
 from utils.pitch_utils import interp_f0
+from utils.infer_utils import resample_align_curve
 
 class RMVPE(BasePE):
     def __init__(self, model_path):
         self.rmvpe = rmvpe(model_path, hop_length=160)
         
     def get_pitch(self, waveform, length, hparams, interp_uv=False, speed=1):
+        f0 = self.rmvpe.infer_from_audio(waveform, sample_rate=hparams['audio_sample_rate'])
+        uv = f0 == 0
+        f0, uv = interp_f0(f0, uv)
+        
         hop_size = int(np.round(hparams['hop_size'] * speed))
         time_step = hop_size / hparams['audio_sample_rate']
-        f0 = self.rmvpe.infer_from_audio(waveform, sample_rate=hparams['audio_sample_rate'])
-        f0 = np.array([f0[int(min(int(np.round(n * time_step / 0.01)), len(f0) - 1))] for n in range(length)])
-        uv = f0 == 0
-        if interp_uv:
-            f0, uv = interp_f0(f0, uv)
-        return f0, uv
+        f0_res = resample_align_curve(f0, 0.01, time_step, length)
+        uv_res = resample_align_curve(uv.astype(float), 0.01, time_step, length) > 0.5
+        if not interp_uv:
+            f0_res[uv_res] = 0
+        return f0_res, uv_res
