5a591de274b7788d4ba4bf37b7dab5ecd90b6fca
yqzhishen <yangqian_1015@icloud.com>
2023-08-09 20:16:31 +0800

    Add error message if attribute is missing

--- preprocessing/variance_binarizer.py
+++ preprocessing/variance_binarizer.py
@@ -71,7 +71,7 @@ class VarianceBinarizer(BaseBinarizer):
             if ds_path.exists():
                 cache_key = item_name_with_idx
             else:
-                ds_path  = self.raw_data_dirs[ds_id] / 'ds' / f'{name}.ds'
+                ds_path = self.raw_data_dirs[ds_id] / 'ds' / f'{name}.ds'
                 cache_key = item_name
             if not ds_path.exists():
                 return None
@@ -89,37 +89,40 @@ class VarianceBinarizer(BaseBinarizer):
         for utterance_label in csv.DictReader(
                 open(raw_data_dir / 'transcriptions.csv', 'r', encoding='utf8')
         ):
+            utterance_label: dict
             item_name = utterance_label['name']
             item_idx = int(item_name.rsplit(DS_INDEX_SEP, maxsplit=1)[-1]) if DS_INDEX_SEP in item_name else 0
 
-            def fallback(attr):
+            def require(attr):
                 if self.prefer_ds:
                     value = self.load_attr_from_ds(ds_id, item_name, attr, item_idx)
                 else:
                     value = None
-                if value is not None:
-                    return value
-                return utterance_label[attr]
+                if value is None:
+                    value = utterance_label.get(attr)
+                if value is None:
+                    raise ValueError(f'Missing required attribute {attr} of item \'{item_name}\'.')
+                return value
 
             temp_dict = {
                 'ds_idx': item_idx,
                 'spk_id': spk_id,
                 'wav_fn': str(raw_data_dir / 'wavs' / f'{item_name}.wav'),
-                'ph_seq': fallback('ph_seq').split(),
-                'ph_dur': [float(x) for x in fallback('ph_dur').split()]
+                'ph_seq': require('ph_seq').split(),
+                'ph_dur': [float(x) for x in require('ph_dur').split()]
             }
 
             assert len(temp_dict['ph_seq']) == len(temp_dict['ph_dur']), \
                 f'Lengths of ph_seq and ph_dur mismatch in \'{item_name}\'.'
 
             if hparams['predict_dur']:
-                temp_dict['ph_num'] = [int(x) for x in fallback('ph_num').split()]
+                temp_dict['ph_num'] = [int(x) for x in require('ph_num').split()]
                 assert len(temp_dict['ph_seq']) == sum(temp_dict['ph_num']), \
                     f'Sum of ph_num does not equal length of ph_seq in \'{item_name}\'.'
 
             if hparams['predict_pitch']:
-                temp_dict['note_seq'] = fallback('note_seq').split()
-                temp_dict['note_dur'] = [float(x) for x in fallback('note_dur').split()]
+                temp_dict['note_seq'] = require('note_seq').split()
+                temp_dict['note_dur'] = [float(x) for x in require('note_dur').split()]
                 assert len(temp_dict['note_seq']) == len(temp_dict['note_dur']), \
                     f'Lengths of note_seq and note_dur mismatch in \'{item_name}\'.'
                 assert any([note != 'rest' for note in temp_dict['note_seq']]), \
