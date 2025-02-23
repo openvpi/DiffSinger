84c1781dcbac4afe38ac870abb0774d519d040d7
Li lingyi <145204041+contiunity@users.noreply.github.com>
2025-02-23 16:11:09 +0800

    Update __init__.py

--- modules/backbones/__init__.py
+++ modules/backbones/__init__.py
@@ -1,11 +1,13 @@
 import torch.nn
 from modules.backbones.wavenet import WaveNet
 from modules.backbones.lynxnet import LYNXNet
+from modules.backbones.neoformer import DiffsingerNeoformer
 from utils import filter_kwargs
 
 BACKBONES = {
     'wavenet': WaveNet,
     'lynxnet': LYNXNet
+    'neoformer': DiffsingerNeoformer
 }
 
 
