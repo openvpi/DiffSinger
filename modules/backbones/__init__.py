fbae4daa974165cad877705f97e8433a6cd07cfe
Li lingyi <145204041+contiunity@users.noreply.github.com>
2025-04-06 13:42:23 +0800

    Update __init__.py

--- modules/backbones/__init__.py
+++ modules/backbones/__init__.py
@@ -1,11 +1,13 @@
 import torch.nn
 from modules.backbones.wavenet import WaveNet
 from modules.backbones.lynxnet import LYNXNet
+from modules.backbones.asivonet import AsivoNet
 from utils import filter_kwargs
 
 BACKBONES = {
     'wavenet': WaveNet,
-    'lynxnet': LYNXNet
+    'lynxnet': LYNXNet,
+    'asivonet': AsivoNet
 }
 
 
