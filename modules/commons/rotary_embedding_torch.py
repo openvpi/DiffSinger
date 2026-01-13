9bb3a9fe8a267aba5e474e5e8cb71a415fc41ef8
yqzhishen <yangqian_1015@icloud.com>
2026-01-13 15:51:25 +0800

    Compute RoPE cache in init and make it read-only
    (cherry picked from commit 4e298fe7e7178810660a6d53eb625acc0a549a40)

--- modules/commons/rotary_embedding_torch.py
+++ modules/commons/rotary_embedding_torch.py
@@ -1,8 +1,7 @@
-from __future__ import annotations
 import torch
-from torch import nn, einsum, Tensor
-from torch.nn import Module
 from einops import rearrange, repeat
+from torch import einsum, Tensor
+from torch.nn import Module
 
 
 def rotate_half(x: Tensor, interleaved=True) -> Tensor:
@@ -22,56 +21,43 @@ def apply_rotary_emb(freqs: Tensor, t: Tensor, interleaved=True) -> Tensor:
     rot_dim = freqs.shape[-1]
     t_to_rotate = t[..., :rot_dim]
     t_pass_through = t[..., rot_dim:]
-    
+
     t_rotated = (t_to_rotate * freqs.cos()) + (rotate_half(t_to_rotate, interleaved) * freqs.sin())
-    
+
     return torch.cat((t_rotated, t_pass_through), dim=-1)
 
 
 class RotaryEmbedding(Module):
     def __init__(
-        self,
-        dim,
-        theta=10000,
-        precompute_len=8192,
-        cache_max_seq_len=8192,
-        interleaved: bool = True
+            self,
+            dim,
+            theta=10000,
+            max_seq_len=8192,
+            interleaved: bool = True
     ):
         super().__init__()
         self.interleaved = interleaved
-
+        self.cached_freqs_seq_len = max_seq_len
         inv_freq = 1. / (theta ** (torch.arange(0, dim, 2).float() / dim))
-        self.register_buffer('inv_freq', inv_freq)
-
-        self._cache_max_seq_len = max(precompute_len, cache_max_seq_len)
-        self._precomputed_len = precompute_len
-
-        self.register_buffer('cached_freqs', None, persistent=False)
-        self.cached_freqs_seq_len = 0
-        
-        if self._precomputed_len > 0:
-            self._precompute_cache(self._precomputed_len)
+        self.register_buffer('inv_freq', inv_freq, persistent=False)
+        self.register_buffer('cached_freqs', self._precompute_cache(max_seq_len), persistent=False)
 
     def _precompute_cache(self, seq_len: int):
         seq = torch.arange(seq_len, device=self.inv_freq.device, dtype=self.inv_freq.dtype)
         freqs = einsum('i, j -> i j', seq, self.inv_freq)
-        
         if self.interleaved:
             freqs = repeat(freqs, '... n -> ... (n r)', r=2)
         else:
             freqs = torch.cat((freqs, freqs), dim=-1)
+        return freqs
 
-        self.cached_freqs = freqs
-        self.cached_freqs_seq_len = seq_len
-
-    def forward(self, t: Tensor, seq_len: int) -> Tensor:
-        if self.cached_freqs is None or seq_len > self.cached_freqs_seq_len:
-            self._precompute_cache(seq_len)
-        
+    def forward(self, seq_len: int) -> Tensor:
+        if seq_len > self.cached_freqs_seq_len:
+            raise RuntimeError("sequence exceeds RoPE max_seq_len!")
         return self.cached_freqs[0: seq_len].detach()
 
     def rotate_queries_or_keys(self, t: Tensor) -> Tensor:
         device, dtype, seq_len = t.device, t.dtype, t.shape[-2]
-        freqs = self.forward(t, seq_len=seq_len)
-        
+        freqs = self.forward(seq_len=seq_len)
+
         return apply_rotary_emb(freqs.to(device=device, dtype=dtype), t, self.interleaved)
