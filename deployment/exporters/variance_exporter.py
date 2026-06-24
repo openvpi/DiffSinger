8e61d609b78a3940a4ad5e1e85c57bde875d6aa0
Kakaru <97896816+KakaruHayate@users.noreply.github.com>
2026-06-25 00:15:34 +0800

    Update ignored patterns in variance exporter

--- deployment/exporters/variance_exporter.py
+++ deployment/exporters/variance_exporter.py
@@ -759,14 +759,15 @@ class DiffSingerVarianceExporter(BaseExporter):
         var_post = onnx_helper.simplify_onnx(var_post)
 
         ignored_variance_names = '|'.join([f'({v_name})' for v_name in self.model.variance_prediction_list])
+        ignored_variance_pred_names = '|'.join([f'({v_name}_pred)' for v_name in self.model.variance_prediction_list])
         onnx_helper.model_add_prefixes(
             var_pre, node_prefix='/pre', value_info_prefix='/pre', initializer_prefix='/pre',
-            ignored_pattern=fr'.*((embed)|{ignored_variance_names}).*'
+            ignored_pattern=fr'.*((embed)|(variance_cond)|{ignored_variance_names}).*'
         )
         onnx_helper.model_add_prefixes(var_pre, dim_prefix='pre.', ignored_pattern='(n_tokens)|(n_frames)')
         onnx_helper.model_add_prefixes(
             var_post, node_prefix='/post', value_info_prefix='/post', initializer_prefix='/post',
-            ignored_pattern=None
+            ignored_pattern=fr'.*({ignored_variance_pred_names}).*'
         )
         onnx_helper.model_add_prefixes(var_post, dim_prefix='post.', ignored_pattern='n_frames')
 
