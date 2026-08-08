f5ba7b633166575c857164f39cbd4ebdea63d0b4
KakaruHayate <kakaru.hk@hotmail.com>
2026-08-08 11:17:59 +0800

    feat: expose RoPE theta via config API

--- modules/fastspeech/acoustic_encoder.py
+++ modules/fastspeech/acoustic_encoder.py
@@ -58,6 +58,7 @@ class FastSpeech2Acoustic(nn.Module):
             dropout=hparams['dropout'], num_heads=hparams['num_heads'],
             use_pos_embed=hparams['use_pos_embed'], rel_pos=hparams.get('rel_pos', False),
             use_rope=hparams.get('use_rope', False), rope_interleaved=hparams.get('rope_interleaved', True),
+            rope_theta=hparams.get('rope_theta', 10000),
             mix_ln_layer=self.mix_ln_layer
         )
 
