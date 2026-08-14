correct the length calculation

preprocessing/variance_binarizer.py
@@ -275,7 +275,6 @@ class VarianceBinarizer(BaseBinarizer):
         ds_id = int(ds_id)
         ds_seg_idx = meta_data['ds_idx']
         seconds = sum(meta_data['ph_dur'])
-        length = round(seconds / self.timestep)
         T_ph = len(meta_data['ph_seq'])
         processed_input = {
             'name': item_name,
@@ -283,7 +282,6 @@ class VarianceBinarizer(BaseBinarizer):
             'spk_id': meta_data['spk_id'],
             'spk_name': meta_data['spk_name'],
             'seconds': seconds,
-            'length': length,
             'languages': np.array(meta_data['lang_seq'], dtype=np.int64),
             'tokens': np.array(meta_data['ph_seq'], dtype=np.int64),
             'ph_text': meta_data['ph_text'],
@@ -292,7 +290,9 @@ class VarianceBinarizer(BaseBinarizer):
         ph_dur_sec = torch.FloatTensor(meta_data['ph_dur']).to(self.device)
         ph_acc = torch.round(torch.cumsum(ph_dur_sec, dim=0) / self.timestep + 0.5).long()
         ph_dur = torch.diff(ph_acc, dim=0, prepend=torch.LongTensor([0]).to(self.device))
+        length = int(ph_acc[-1])
         processed_input['ph_dur'] = ph_dur.cpu().numpy()
+        processed_input['length'] = length
 
         mel2ph = get_mel2ph_torch(
             self.lr, ph_dur_sec, length, self.timestep, device=self.device